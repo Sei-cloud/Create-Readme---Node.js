b437aa0d16e6ec6c4cdd7a3fdd62bac92566f687
Sei Munkhbat <sei@Seis-MacBook-Pro.local>
2024-04-25 10:47:14 -0500

    Added Functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,74 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the testing instructions for your project?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, err => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log(`${filename} saved successfully!`);
+        }
+    });
+}
 
-// Function call to initialize app
+// Function to initialize program
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const markdownContent = generateMarkdown(answers);
+            writeToFile('README.md', markdownContent);
+        })
+        .catch(error => {
+            console.error('Error occurred:', error);
+        });
+}
+
+// Function call to initialize program
 init();
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Create-Readme---Node.js
+
+![Video-Tutorial]('./Develop/Assets/Video-Tutorial.mov')
